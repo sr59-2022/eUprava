896b6cbbb8ded5a79d07ba1cda6fe994b5d54861
Sara Zivanovic <sarazivanovic755@gmail.com>
2026-01-12 20:09:48 +0100

    docker setup

---
+++ fakultet-service/src/main/java/com.example.fakultet/FakultetServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.fakultet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakultetServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FakultetServiceApplication.class, args);
+    }
+}
---
+++ sluzbaZaZaposljavanje-service/src/main/java/com/example/sluzba/model/SluzbaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.example.sluzba.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluzbaServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SluzbaServiceApplication.class, args);
+    }
+}
